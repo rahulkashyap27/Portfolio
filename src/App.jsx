09cbeff0b190fe5c0704183a7ab760515c3f68f5
Rahul Kashyap <rahulkashyap.83039@gmail.com>
2024-03-12 15:42:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <h1> Hii Rahul Kashyap this side.</h1>
+    <h3> This Site is under contruction...</h3>
+    <a href="https://www.linkedin.com/in/Rahul-Kashyap27/">Connect me on LinkedIn</a>
+  </div>
+  )
+}
+
+export default App
